d1980c613fd46fb3f208bf9c4edfce7ecba4f4cf
Crat <id2359@yahoo.com>
2015-06-24 23:48:11 +0800

    fixed wrong names

--- monitor.py
+++ monitor.py
@@ -2,20 +2,20 @@ import clips
 from dispatch import Dispatcher, DispatchError
 from retrievers import RetrievalError
 
-BAD_VALUE = clips.Symbol("bad")
+NULL_VALUE = clips.Symbol("null")
 
 def get_value(name):
     print "get_value: %s" % name
     try:
-        value = Retriever().get_value(name)
+        value = Dispatcher().get_value(name)
         print "got value: %s" % value
         return value
     except RetrievalError:
         print "retrieval error!"
-        return BAD_VALUE
+        return NULL_VALUE
     except DispatchError:
         print "dispatch error!"
-        return BAD_VALUE
+        return NULL_VALUE
 
 if __name__=="__main__":
     clips.RegisterPythonFunction(get_value, "python-get-value")
