b5b5e910cecfda7c05221469c7da3d72f79db6fb
Crat <id2359@yahoo.com>
2015-07-19 21:48:25 +0800

    made dispatcher attempt to run external command

--- dispatch.py
+++ dispatch.py
@@ -1,6 +1,5 @@
-import inspect
-import importlib
 import retrievers
+import os
 
 class DispatchError(Exception):
     pass
@@ -12,5 +11,9 @@ class Dispatcher(object):
         if hasattr(retrievers, python_name):
             func = getattr(retrievers, python_name)
             return func()
+        elif os.path.exists(name):
+            result = float(os.popen(name).read().strip())
+
+            return result
         else:
             raise DispatchError(name)
